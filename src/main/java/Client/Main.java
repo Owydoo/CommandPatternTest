7f8ddfa1107d389f26920cbb03542b6940cc359d
Hersemeule <h.hersemeule@lombardodier.com>
2021-11-03 18:02:57 +0100

    Using method references

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -8,6 +8,7 @@ import Receiver.TextFile;
 public class Main {
     public static void main(String[] args) {
         TextFileOperationExecutor textFileOperationExecutor = new TextFileOperationExecutor();
+
         System.out.println(textFileOperationExecutor.executeOperation(new OpenTextFileOperation(new TextFile("file1.txt"))));
         System.out.println(textFileOperationExecutor.executeOperation(new SaveTextFileOperation(new TextFile("file2.txt"))));
 
@@ -15,6 +16,9 @@ public class Main {
         System.out.println(textFileOperationExecutor.executeOperation(() -> "Opening file file1.txt"));
         System.out.println(textFileOperationExecutor.executeOperation(() -> "Opening file file2.txt"));
 
-        //
+        //Using method references
+        TextFile textFile = new TextFile("file1.txt");
+        System.out.println(textFileOperationExecutor.executeOperation(textFile::open));
+        System.out.println(textFileOperationExecutor.executeOperation(textFile::save));
     }
 }
